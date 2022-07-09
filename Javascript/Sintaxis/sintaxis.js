1a67eb6ea26d36c01e1039ace3e6e67a392e9f26
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-07-08 22:53:11 -0300

    Resolucion Ejercicio - Sintaxis en Javascript

---
+++ Javascript/Sintaxis/sintaxis.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Marcos Cruz",
+    35,
+    true, 
+    new Date (1986 / 10 / 11),
+    {
+    titulo: "PADRE RICO PADRE POBRE (EDICION 20 AÑOS)",
+    autor: "Kiyosaki, Robert T.",
+    fecha: new Date(2018/01/01),
+    url: "https://articulo.mercadolibre.com.ar/MLA-811576294-padre-rico-padre-pobre-edicion-20-anos-kiyosaki-robert-_JM#position=2&search_layout=stack&type=pad&tracking_id=3aa23f46-a409-4443-9a64-6a44acb16312#position=2&search_layout=stack&type=pad&tracking_id=3aa23f46-a409-4443-9a64-6a44acb16312&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=2&ad_click_id=NGIyYzg3YTctZmM1Ny00YmU5LWJhOTctMjMwNTYwZTNiZWM2"
+    }
+]
+
+
