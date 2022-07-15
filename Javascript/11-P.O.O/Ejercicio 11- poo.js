e7f57a1802ce066b4739641b0f8563f55a55c4d9
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-07-15 12:56:37 -0300

    Resolución ejercicio 11 - Objetos y clases

---
+++ Javascript/11-P.O.O/Ejercicio 11- poo.js
@@ -0,0 +1,23 @@
+/** Crea un nuevo fichero JS que contenga las siguientes líneas
+
+- Una clase llamada "Estudiante" que tenga:
+- Una variable llamada nombre
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+- Crea una nueva instancia de "Estudiante"
+- Haz la llamada al método obtenDatos
+ */
+
+class Estudiante {
+    constructor (nombre) { 
+        this.nombre = nombre,
+        this.asignaturas = ["Javascript", "HTML", "CSS"]
+    }
+    obtenDatos () {
+        console.log(`"Hola soy ${this.nombre} y mis lenguajes de programacion son ${this.asignaturas.join(", ")}`)
+    }
+}
+
+const nuevoEstudiante = new Estudiante("Marcos")
+
+console.log(nuevoEstudiante.obtenDatos())
