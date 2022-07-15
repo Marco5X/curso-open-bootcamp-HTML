1e369f8be455250edd45ae0b36516e4278f4464a
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-07-15 17:17:31 -0300

    Resolucion ejercicio 12 - Depuracion en VSC

---
+++ Javascript/12-Depuracion/Ejercicio12- depuracion.js
@@ -0,0 +1,19 @@
+/** Crea un nuevo fichero JS que contenga las siguientes líneas
+
+- Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+- Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+ */
+
+function generaFibonacci(num) {
+    let fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (let i = 2; i < num; i++) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci;
+}
+let fibo = generaFibonacci(10);
+console.log(fibo);
+
+ 
